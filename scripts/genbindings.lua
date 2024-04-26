a84a9d25c809dd40ce94a4d53768a7358c7d4c27
komothecat <71205197+komothecat@users.noreply.github.com>
2024-04-26 22:55:41 +0800

    fix(3rd/curl): add preproc that include libcurl

--- scripts/genbindings.lua
+++ scripts/genbindings.lua
@@ -1,15 +1,19 @@
 local nldecl = require 'nelua.plugins.nldecl'
 local fs = require 'nelua.utils.fs'
 
-if not fs.isfile('ez/3rd/curl-bindings.nelua') then
-    nldecl.generate_bindings_file{
-        output_file = 'ez/3rd/curl-bindings.nelua',
-        parse_includes = {'<curl/curl.h>'},
-        include_names = {
-            '^curl',
-            '^CURL',
-            sockaddr = true,
-            fd_set = true,
-        }
+nldecl.generate_bindings_file{
+    output_head = [=[
+##[[
+cinclude "<curl/curl.h>"
+linklib "curl"
+]]
+]=],
+    output_file = 'ez/3rd/curl-bindings.nelua',
+    parse_includes = {'<curl/curl.h>'},
+    include_names = {
+        '^curl',
+        '^CURL',
+        sockaddr = true,
+        fd_set = true,
     }
-end
+}
